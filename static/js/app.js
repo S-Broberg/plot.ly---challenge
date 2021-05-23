9b2ac6cb53105f27e4fe5999f0d8f61b68f6cc5c
S-Broberg <78188204+S-Broberg@users.noreply.github.com>
2021-05-23 00:01:50 -0700

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,7 @@
-// Test Subject ID No. menu and default plots
 function getName() {
-    // Select dropdown menu id and assign it to a variable
+    
     var dropdownMenu = d3.select('#selDataset');
-    // Read "names" values from json file and append into dropdown menu
+    
     d3.json('data/samples.json')
         .then(subject => subject.names
         .forEach(name => dropdownMenu
@@ -10,7 +9,7 @@ function getName() {
             .text(name)
             .property('value'),
 
-            // Initialize page with default metadata and plots   
+             
             getMetadata(subject.names[0]),
             getBar(subject.names[0]),
             getBubble(subject.names[0]),
@@ -19,7 +18,6 @@ function getName() {
     );
 };
 
-// Function called by DOM changes
 function optionChanged(id) {
     getMetadata(id);
     getBar(id);
@@ -27,19 +25,19 @@ function optionChanged(id) {
     getGauge(id);
 };
 
-// Demographic Info
+
 function getMetadata(id) {
-    // Read "metadata" from json file for each subject and assign it to a variable
+    
     d3.json('data/samples.json')
         .then(data => {
         var subjectData = data.metadata
         .filter(subject => subject.id.toString() === id)[0];
         
-        // Select demographic info id and assign it to a variable
+        
         var subjectMetadata = d3.select('#sample-metadata');
-        // Clear metadata befor displaying new selection data 
+        
         subjectMetadata.html('');
-        // Push data into demographic info card
+        
         Object.entries(subjectData)
             .forEach(([key, value]) => subjectMetadata
             .append('p')
@@ -50,15 +48,15 @@ function getMetadata(id) {
 
 getName();
 
-// Bar chart
+
 function getBar(id) {
-    // Read data from json file for each sample, assign it to a variable, and plot it
+    
     d3.json('data/samples.json')
         .then(data => {
         var sortedSample = data.samples
         .filter(sample => sample.id === id)[0];
         console.log(sortedSample);
-        // Trace for bar chart that displays each sample top 10 OTU values
+        
         var barTrace = {
             x: sortedSample.sample_values.slice(0,10).reverse(),
             y: sortedSample.otu_ids.slice(0,10).map(otuid => `OTU ${otuid}`).reverse(),
@@ -82,9 +80,9 @@ function getBar(id) {
             type: 'bar',
             orientation: 'h'
         };
-        // Data
+        
         var data = [barTrace];
-        // Layout
+        
         var layout = {
             title: {
                 text: `Top 10 OTU for Test Subject No. ${id}`,
@@ -94,7 +92,7 @@ function getBar(id) {
                     color: 'black'
                 },
             },
-            //margin: {l: 100, r: 50, t: 50, b: 50},
+            
             height: 600,
             width: 630,
             xaxis: {
@@ -125,7 +123,7 @@ function getBar(id) {
                 },
             },
         };
-        // Render the plot to the div tag with id 'bar'
+        
         Plotly.newPlot('bar', data, layout, {
             modeBarButtonsToRemove: [
                 'zoom2d',
@@ -140,16 +138,16 @@ function getBar(id) {
     });
 };
 
-// Bubble chart
+
 function getBubble(id) {
-    // Read data from json file for each sample, assign it to a variable, and plot it
+    
     d3.json('data/samples.json')
         .then(data => {
         var sortedSample = data.samples
         .filter(sample => sample.id === id)[0];
         console.log(sortedSample);
 
-        // Trace for bubble chart that displays each sample values
+       
         var bubbleTrace = {
             x: sortedSample.otu_ids,
             y: sortedSample.sample_values,
@@ -177,9 +175,9 @@ function getBubble(id) {
                 }
             },
         };
-        // Data
+        
         var data = [bubbleTrace];
-        // Layout
+        
         var layout = {
             title: {
                 text: `Test Subject No. ${id} Belly Button Biodiversity`,
@@ -189,7 +187,7 @@ function getBubble(id) {
                     color: 'black'
                 },
             },
-            //margin: {l: 50, r: 100, t: 30, b: 30},
+            
             height: 520,
             width: 920,
             xaxis: {
@@ -218,7 +216,7 @@ function getBubble(id) {
                 },
             },
         };
-        // Render the plot to the div tag with id 'bubble'
+        
         Plotly.newPlot('bubble', data, layout, {
             modeBarButtonsToRemove: [
                 'zoom2d',
@@ -233,19 +231,19 @@ function getBubble(id) {
     });
 };
 
-// Gauge chart
+
 function getGauge(id) {
-    // Read "metadata" from json file for each subject and assign it to a variable
+    
     d3.json('data/samples.json')
         .then(data => {
         var subjectData = data.metadata
         .filter(subject => subject.id.toString() === id)[0];
         console.log(subjectData);
 
-        // Extract "wfreq" data and assign it to a variable to build an indicator
+        
         var value = subjectData.wfreq;
         console.log(value);
-        // Data
+        
         var data = [
             {
                 domain: { x: [0, 1], y: [0, 1] },
@@ -276,14 +274,14 @@ function getGauge(id) {
                 },
             },
         ];
-        // Layout 
+        
         var layout = {
             width: 440,
             height: 360,
             margin: { t: 35, r: 15, l: 15, b: 0},
             font: { color: 'darkgrey', family: 'Arial'}
         };
-        // Render the plot to the div tag with id 'gauge'
+        
         Plotly.newPlot('gauge', data, layout);
     });
 };
